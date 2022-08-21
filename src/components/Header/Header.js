e2e26522a7329d351702b3584773566c590e3083
Shafiq Shams <sshams@north.io>
2022-08-21 18:16:50 +0200

    Line component moved into Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,19 +3,22 @@ import React from 'react';
 import styles from './HeaderStyles';
 import HeaderTitle from '../../assets/icons-logo/eventlocations.svg';
 import MenuIcon from '../../assets/icons-logo/menu.svg';
-
+import Line from '../Line/Line';
 import colors from '../../styles/colors';
 
 const Header = () => {
   return (
-    <View style={styles.mainWrapper}>
-      <View style={styles.headerContainer}>
-        <HeaderTitle width={140} height={75} color={colors.black01} />
+    <>
+      <View style={styles.mainWrapper}>
+        <View style={styles.headerContainer}>
+          <HeaderTitle width={140} height={75} color={colors.black01} />
+        </View>
+        <TouchableOpacity style={styles.iconContainer}>
+          <MenuIcon width={35} height={35} color={colors.black01} />
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.iconContainer}>
-        <MenuIcon width={35} height={35} color={colors.black01} />
-      </TouchableOpacity>
-    </View>
+      <Line />
+    </>
   );
 };
 
