dd8b424684bfe6ca756fc8c38ab5ddd4527ea0b2
Shafiq Shams <sshams@north.io>
2022-08-21 22:31:36 +0200

    Colors updated

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -5,9 +5,11 @@ const colors = {
   grey01: '#B7B7B7',
   grey02: '#949494',
   grey03: '#c4c4c4',
+  grey04: '#545454',
   red: '#fc4c56',
   yellow: '#ecc94a',
-  lightblue: '#369bd1',
+  lightblue01: '#319bd2',
+  lightblue02: '#1a71a5',
 };
 
 export default colors;
