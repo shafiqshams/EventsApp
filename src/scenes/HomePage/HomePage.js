8be4618bc963acddc27bdc3976d24ff8680ed67f
Shafiq Shams <sshams@north.io>
2022-08-22 01:54:38 +0200

    Fetching venues in Home scene

--- src/scenes/HomePage/HomePage.js
+++ src/scenes/HomePage/HomePage.js
@@ -1,19 +1,34 @@
-import {View, Text} from 'react-native';
-import React, {useEffect} from 'react';
+import {SafeAreaView, Text, FlatList} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import styles from './HomePageStyles';
 import Header from '../../components/Header/Header';
 import VenueCard from '../../components/VenueCard/VenueCard';
+import {getVenues} from '../../services/events/events';
 
-const HomePage = ({navigation}) => {
+const HomePage = () => {
+  const [venues, setVenues] = useState([]);
+
   useEffect(() => {
-    console.log('HomePage Mounted');
+    const fetchVenues = async () => {
+      const {data} = await getVenues();
+      setVenues(data);
+    };
+
+    fetchVenues();
   }, []);
 
   return (
-    <View style={styles.mainWrapper}>
+    <SafeAreaView style={styles.mainWrapper}>
       <Header />
-      <VenueCard />
-    </View>
+      <FlatList
+        data={venues}
+        renderItem={({item}) => <VenueCard venue={item} />}
+        keyExtractor={item => `key-${item.id}`}
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        // alwaysBounceVertical={false}
+      />
+    </SafeAreaView>
   );
 };
 
