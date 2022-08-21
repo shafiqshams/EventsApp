005ae99f7770e87568d47ffc85ebc7df3f7f0281
Shafiq Shams <sshams@north.io>
2022-08-22 01:54:12 +0200

    API Wrapper added for GET

---
+++ src/services/helpers/api.js
@@ -0,0 +1,17 @@
+export const getFetch = (url, params = {}) => {
+  const queryString = Object.entries(params)
+    .map(param => {
+      return `${param[0]}=${param[1]}`;
+    })
+    .join('&');
+
+  return fetch(`${url}?${queryString}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .catch(err => console.error(err));
+};
