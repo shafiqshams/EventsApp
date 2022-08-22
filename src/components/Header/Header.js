2f1701b9984443821442324c2cddf64d179e59e2
Shafiq Shams <sshams@north.io>
2022-08-23 01:18:05 +0200

    Header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,25 @@ import {TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import styles from './HeaderStyles';
 import HeaderTitle from '../../assets/icons-logo/eventlocations.svg';
-import MenuIcon from '../../assets/icons-logo/menu.svg';
+import Bookmark from '../../assets/icons-logo/bookmarks.svg';
+import Back from '../../assets/icons-logo/arrow-back.svg';
+
 import Line from '../Line/Line';
 import colors from '../../styles/colors';
 
-const Header = () => {
+const Header = ({showFav, onPress}) => {
   return (
     <>
       <View style={styles.mainWrapper}>
         <View style={styles.headerContainer}>
           <HeaderTitle width={140} height={75} color={colors.black01} />
         </View>
-        <TouchableOpacity style={styles.iconContainer}>
-          <MenuIcon width={35} height={35} color={colors.black01} />
+        <TouchableOpacity style={styles.iconContainer} onPress={onPress}>
+          {showFav ? (
+            <Back width={28} height={28} color={colors.black01} />
+          ) : (
+            <Bookmark width={28} height={28} color={colors.black01} />
+          )}
         </TouchableOpacity>
       </View>
       <Line />
