e8c87697b013baa2c6ae1837b740302837229030
Shafiq Shams <sshams@north.io>
2022-08-22 14:36:12 +0200

    Loader and handlers added

--- src/scenes/HomePage/HomePage.js
+++ src/scenes/HomePage/HomePage.js
@@ -1,12 +1,16 @@
-import {SafeAreaView, Text, FlatList} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import {SafeAreaView, FlatList} from 'react-native';
+import React, {useEffect, useState, useReducer} from 'react';
 import styles from './HomePageStyles';
+import R from 'ramda';
 import Header from '../../components/Header/Header';
 import VenueCard from '../../components/VenueCard/VenueCard';
 import {getVenues} from '../../services/events/events';
+import Loader from '../../components/Loader/Loader';
+import useFavorite from '../../hooks/useFavorite';
 
 const HomePage = () => {
   const [venues, setVenues] = useState([]);
+  const [favorites, {setFavItem, removeFavItem, getFavorites}] = useFavorite();
 
   useEffect(() => {
     const fetchVenues = async () => {
@@ -17,17 +21,36 @@ const HomePage = () => {
     fetchVenues();
   }, []);
 
+  const addToFavorite = id => {
+    setFavItem(id);
+  };
+
+  const removeFavorite = id => {
+    removeFavItem(id);
+  };
+
   return (
     <SafeAreaView style={styles.mainWrapper}>
       <Header />
-      <FlatList
-        data={venues}
-        renderItem={({item}) => <VenueCard venue={item} />}
-        keyExtractor={item => `key-${item.id}`}
-        bounces={false}
-        showsVerticalScrollIndicator={false}
-        // alwaysBounceVertical={false}
-      />
+      {R.isEmpty(venues) ? (
+        <Loader />
+      ) : (
+        <FlatList
+          data={venues}
+          renderItem={({item}) => (
+            <VenueCard
+              isFav={!!favorites.find(favs => favs.id === item.id)}
+              venue={item}
+              setFavItem={addToFavorite}
+              removeFavItem={removeFavorite}
+            />
+          )}
+          keyExtractor={item => `key-${item.id}`}
+          bounces={false}
+          showsVerticalScrollIndicator={false}
+          // alwaysBounceVertical={false}
+        />
+      )}
     </SafeAreaView>
   );
 };
