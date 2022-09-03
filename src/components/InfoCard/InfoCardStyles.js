075128cc0864bfd80711a33d4c563bcf5ea7077b
Shafiq Shams <sshams@north.io>
2022-09-03 17:15:10 +0200

    Unused removed

--- src/components/InfoCard/InfoCardStyles.js
+++ src/components/InfoCard/InfoCardStyles.js
@@ -1,6 +1,5 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import {StyleSheet} from 'react-native';
 import colors from '../../styles/colors';
-const {width} = Dimensions.get('window');
 
 export default StyleSheet.create({
   infoWrapper: {
